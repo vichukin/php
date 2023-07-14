1365d6f46cabd3541c5635beb997213ff8056724
The Waggle <artuoh76@gmail.com>
2023-07-14 18:27:24 +0200

    Pr&Dz13

---
+++ PHP_13Meet_11.07/Pr13_11.07/app/Controllers/Home.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+    public function Tours()
+    {
+        $city = $this->request->getGet("City");
+        $stars = $this->request->getGet("Stars");
+        $db = db_connect();
+        $arr = array();
+        if ($city != "" && $stars != "") {
+            $res = $db->query("SELECT H.Id,H.HotelName,H.Description, H.Price, H.Stars, C.Country,Ci.City FROM Hotels H LEFT JOIN Cities Ci on H.CityId=Ci.Id LEFT JOIN Countries C on Ci.CountryId=C.Id WHERE Ci.City='$city' and H.Stars=$stars");
+            $message = "Hotels in $city with $stars ".($stars == 1 ? "star" : "stars");
+        } else if ($city != "") {
+            $res = $db->query("SELECT H.Id,H.HotelName,H.Description, H.Price, H.Stars, C.Country,Ci.City FROM Hotels H LEFT JOIN Cities Ci on H.CityId=Ci.Id LEFT JOIN Countries C on Ci.CountryId=C.Id WHERE Ci.City='$city'");
+            $message = "Hotels in $city";
+        } else if ($stars != "") {
+            $res = $db->query("SELECT H.Id,H.HotelName,H.Description, H.Price, H.Stars, C.Country,Ci.City FROM Hotels H LEFT JOIN Cities Ci on H.CityId=Ci.Id LEFT JOIN Countries C on Ci.CountryId=C.Id WHERE  H.Stars=$stars");
+
+            $message = "Hotels with $stars ".($stars == 1 ? "star" : "stars");
+        } else {
+            $res = $db->query("SELECT H.Id,H.HotelName,H.Description, H.Price, H.Stars, C.Country,Ci.City FROM Hotels H LEFT JOIN Cities Ci on H.CityId=Ci.Id LEFT JOIN Countries C on Ci.CountryId=C.Id");
+            $message = "";
+        }
+        $arr["message"] = $message;
+        if ($res->getNumRows() != 0) {
+            $row = $res->getFirstRow("array");
+            do {
+
+                $buf = $db->query("SELECT I.ImagePath FROM Hotels H left join Images I on I.HotelId=H.Id where H.Id=$row[Id] LIMIT 1");
+                $image = $buf->getFirstRow("array");
+                if ($image["ImagePath"] != null) {
+                    if (!file_exists($image["ImagePath"]))
+                        copy("D:/OSPanel/domains/php/TravelAgency/$image[ImagePath]", $image["ImagePath"]);
+                    $row["image"] = "$image[ImagePath]";
+                } else
+                    $row["image"] = "Images/nf.jpg";
+                $arr["hotels"][] = $row;
+            } while ($row = $res->getNextRow("array"));
+        }
+        $res = $db->query("SELECT * FROM Cities");
+        $row = $res->getFirstRow();
+        do {
+            $arr["Cities"][] = $row->City;
+        } while ($row = $res->getNextRow());
+        return view("Header") . view("Tours", $arr);
+    }
+}
---
+++ PHP_13Meet_11.07/Pr13_11.07/app/Views/Tours.php
@@ -0,0 +1,74 @@
+<?
+
+?>
+<style>
+            .checked {
+                color: orange;
+            }
+
+            #stars {
+                width: max-content;
+                height: max-content;
+            }
+        </style>
+<div class="container">
+
+    <form method="GET">
+        <div class="d-flex mb-4 bg-secondary w-100 p-2">
+            <h2 class="w-25 text-white">Tours</h2>
+            <select name="City" class='form-select w-25'>
+                <option value="">Choose city</option>
+                <?
+                foreach ($Cities as $elem) {
+                    echo "<option >$elem</option>";
+                }
+                ?>
+            </select>
+            <select name="Stars" class='form-select w-25 ms-2'>
+                <option value="">Choose stars</option>
+                <?
+                foreach (range(1, 5) as $elem) {
+                    echo "<option value='$elem'>$elem " . ($elem == 1 ? "Star" : "Stars") . "</option>";
+                }
+                ?>
+            </select>
+            <button class="btn btn-primary w-25 ms-2">Search</button>
+        </div>
+    </form>
+    <?
+    echo $message==""?"":"<h4>$message:</h4>";
+    ?>
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 m-2">
+
+        <?
+        
+        if (isset($hotels)) {
+            foreach ($hotels as $elem) {
+        ?>
+                <div class="card mb-3">
+                    <img src="<? echo $elem["image"] ?>" style='object-fit: cover; width: 100%;height: 25vh;' class="card-img-top" alt="photo">
+                    <div class="card-body">
+                        <h5 class="card-title"><? echo $elem["HotelName"] ?></h5>
+                        <p class="card-text"><? echo $elem["Description"] ?></p>
+                        <div class="d-flex justify-content-end">
+                            <p class="card-text me-auto"><small class="text-body-secondary d-flex"><? echo $elem["City"] ?>, <? echo $elem["Country"] ?></small></p>
+                            <div id="stars">
+                                    <?
+                                        foreach(range(1,$elem["Stars"])as $star)
+                                        {
+                                            echo "<p class='fa fa-star checked' id='star$star'></p>";
+                                        }
+                                        foreach(range($elem["Stars"]+1,5)as $star)
+                                        {
+                                            echo "<p class='fa fa-star' id='star$star'></p>";
+                                        }
+                                    ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        <?
+            }
+        } ?>
+    </div>
+</div>
